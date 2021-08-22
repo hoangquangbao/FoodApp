a22a9c0348ae599335b11217116539b743ce37d2
Hoang Quang Bao <hoangquangbao.developer@gmail.com>
2021-08-22 22:44:20 +0700

    Initial Commit

---
+++ FoodApp/FoodAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodAppApp.swift
+//  FoodApp
+//
+//  Created by Quang Bao on 22/08/2021.
+//
+
+import SwiftUI
+
+@main
+struct FoodAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
