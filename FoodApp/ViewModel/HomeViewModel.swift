0c6bd2be29bf201f72fef65d5c703d2161874f74
Hoang Quang Bao <hoangquangbao.developer@gmail.com>
2021-08-24 16:36:25 +0700

    Finished about 80% for FoodApp UI

---
+++ FoodApp/ViewModel/HomeViewModel.swift
@@ -0,0 +1,20 @@
+//
+//  HomeViewModel.swift
+//  FoodApp
+//
+//  Created by Quang Bao on 23/08/2021.
+//
+
+import SwiftUI
+
+struct HomeViewModel: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct HomeViewModel_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeViewModel()
+    }
+}
